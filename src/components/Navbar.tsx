e315f4be1c1b6bca5eea1b7b63000ed94179467d
Nihar Hegde <niharhegde163@gmail.com>
2023-11-25 14:49:50 +0530

    finishing up the cart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { Icons } from "./Icons";
 import NavItems from "./NavItems";
 import { buttonVariants } from "./ui/button";
+import Cart from "./Cart";
 
 const Navbar = () => {
   const user = null;
@@ -46,8 +47,7 @@ const Navbar = () => {
                     </div>
                   }
                   <div className="ml-4 flow-root lg:ml-6">
-
-                    {/*TODO: Add the card element here */}
+                    <Cart />
                   </div>
                 </div>
               </div>
